565397fef34c147a3315ca7b15b3c6fd7666a85d
james.patmore <110008934+jamestpatmore@users.noreply.github.com>
2023-01-11 16:53:12 -0700

    deleted uneeded files also added changes to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
-  // uri: 'http://localhost:6001/graphql'
-  request: operation => {
-    const token = localStorage.getItem('id_token');
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
-  uri: '/graphql'
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
